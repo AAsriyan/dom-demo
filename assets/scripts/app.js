9f1417d936f0ca786dc6f59d3b86e14623d97e77
Alex Asriyan <aasriyan01@gmail.com>
2022-04-06 10:52:04 -0400

    added create functionality to movies and displays them in a list

---
+++ assets/scripts/app.js
@@ -0,0 +1,90 @@
+const d = document;
+const addMovieModal = d.getElementById("add-modal");
+//const addMovieModal = d.querySelector("#add-modal");
+//const addMovieModal = d.body.children[1];
+const startAddMovieButton = d.querySelector("header button");
+const backdrop = d.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+const entryTextSection = d.getElementById("entry-text");
+
+const movies = [];
+
+const updateUI = () => {
+	if (movies.length === 0) {
+		entryTextSection.style.display = "block";
+	} else {
+		entryTextSection.style.display = "none";
+	}
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+	const newMovieElement = d.createElement("li");
+	newMovieElement.className = "movie-element";
+	newMovieElement.innerHTML = `
+		<div class="movie-element__image">
+			<img src="${imageUrl}" alt="${title}">
+		</div>
+		<div class="movie-element__info">
+			<h2>${title}</h2>
+			<p>${rating}/5 stars</p>
+		</div>
+	`;
+
+	const listRoot = d.getElementById("movie-list");
+	listRoot.append(newMovieElement);
+}
+
+const toggleBackdrop = () => {
+	backdrop.classList.toggle("visible");
+}
+
+const toggleMovieModal = () => {
+	addMovieModal.classList.toggle("visible");
+	toggleBackdrop();
+}
+
+const clearMovieInput = () => {
+	for (const userInput of userInputs) {
+		userInput.value = "";
+	}
+}
+
+const cancelAddMovieHandler = () => {
+	toggleMovieModal();
+	clearMovieInput();
+}
+
+const addMovieHandler = () => {
+	const titleValue = userInputs[0].value;
+	const imageUrlValue = userInputs[1].value;
+	const ratingValue = userInputs[2	].value;
+
+	if (titleValue.trim() === "" || imageUrlValue === "" || ratingValue === "" || parseInt(ratingValue) < 1 || parseInt(ratingValue) > 5) {
+		alert("Please enter valid values (rating between 1 and 5)");
+		return;
+	}
+
+	const newMovie = {
+		title: titleValue,
+		image: imageUrlValue,
+		rating: ratingValue
+	};
+
+	movies.push(newMovie);
+	console.log(movies);
+	toggleMovieModal();
+	clearMovieInput();
+	renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+	updateUI();
+}
+
+const backdropClickHandler = () => {
+	toggleMovieModal();
+}
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler)
